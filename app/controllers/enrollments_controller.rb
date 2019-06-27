8ad5a7965300c59f7e865e8f147453bf07a4a59e
jtw1984 <watsonjamest@gmail.com>
2019-06-27 21:43:12 +0000

    Add payments with stripe

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -1,7 +1,28 @@
 class EnrollmentsController < ApplicationController
     def create
+        if current_course.premium?
+
+        # Amount in cents
+        @amount = (current_course.cost * 100).to_i
+
+        customer = Stripe::Customer.create(
+            email: params[:stripeEmail],
+            source: params[:stripeToken]
+            )
+
+        charge = Stripe::Charge.create(
+            customer: customer.id,
+            amount: @amount,
+            description: 'Flixter Premo Content',
+            currency: 'usd'
+             )
+        end
+        
         current_user.enrollments.create(course: current_course)
         redirect_to course_path(current_course)
+        rescue Stripe::CardError => e
+            flash[:error] = e.message
+            redirect_to root_path
     end
 
     private
@@ -10,3 +31,4 @@ class EnrollmentsController < ApplicationController
         @current_course ||= Course.find(params[:course_id])
     end
 end
+
--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,11 +1,31 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, only: [:show]
+
   def show
   end
 
   private
 
+  def require_authorized_for_current_course
+    if current_lesson.section.course.user != current_user
+      redirect_to course_path(current_course), alert: 'You are not enrolled in this course.'
+    end
+  end
+
   helper_method :current_lesson
   def current_lesson
       @current_lesson ||= Lesson.find(params[:id])
   end
+
+  helper_method :current_course
+  def current_course
+    @current_course ||= Course.find(params[:id])
+  end
+
+  def course_params
+      params.require(:course).permit(:title, :description, :cost, :image)
+  end
 end
+
+
